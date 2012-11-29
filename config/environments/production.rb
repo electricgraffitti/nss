e7395f1c60082afb8c0dab1a05f5601fe407058a
Bob Hanson <bob@cube2media.com>
2012-11-29 10:55:15 -0700

    removed EN

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,11 +54,6 @@ Nss::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
-  config.middleware.use ExceptionNotifier,
-    sender_address: 'noreply@cube2media.com',
-    exception_recipients: 'bob@cube2media.com',
-    ignore_crawlers: %w{Googlebot bingbot}
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
